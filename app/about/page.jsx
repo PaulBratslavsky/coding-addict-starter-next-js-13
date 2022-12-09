80856daa225a2071c6d3e833b76baefcff619750
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2022-12-08 21:32:35 -0600

    update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,22 +1,7 @@
-import qs from "qs";
 import PlaceHolder from "../../components/PlaceHolder";
 
-const QUERY = (slug) =>
-  qs.stringify({
-    filters: { slug: slug },
-  });
-
-async function getPage(page) {
-  const BASE_URL = "http://localhost:1337/api/pages";
-  const res = await fetch(BASE_URL + "?" + QUERY(page));
-  const data = await res.json();
-  return data;
-}
 
 export default async function Home() {
-  const { data } = await getPage("about");
-  console.log(data);
-
   return (
     <div>
       <PlaceHolder
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-import qs from "qs";
 import CTA from "../components/CTA";
 import Benefits from "../components/Benefits";
 import Hero from "../components/Hero";
@@ -6,29 +5,7 @@ import Stats from "../components/Stats";
 import Steps from "../components/Steps";
 import Faqs from "../components/Faqs";
 
-const QUERY = (slug) =>
-  qs.stringify({
-    populate: {
-      Blocks: {
-        populate: ["link", "image", "Count"]
-      },
-      Navigation: {
-        populate: ["Link"]
-      },
-    },
-    filters: { slug: slug },
-  });
-
-async function getPage(page) {
-  const BASE_URL = "http://localhost:1337/api/pages";
-  const res = await fetch(BASE_URL + "?" + QUERY(page));
-  const data = await res.json();
-  return data;
-}
-
 export default async function Home() {
-  const { data } = await getPage("home");
-  console.log(data)
 
   return (
     <div>
